a8661a792285aed85a6997f2434fe3bb25ab361a
Raj14204 <83156550+Raj14204@users.noreply.github.com>
2021-05-21 20:33:33 +0530

    Add files via upload

---
+++ IPL.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDrGxMzHF5De1r7ngWvnOkJV878-W46ixM",
+    authDomain: "ipl-356bd.firebaseapp.com",
+    databaseURL: "https://ipl-356bd-default-rtdb.firebaseio.com",
+    projectId: "ipl-356bd",
+    storageBucket: "ipl-356bd.appspot.com",
+    messagingSenderId: "907075621320",
+    appId: "1:907075621320:web:cf899a24422bd5e9f530fd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
